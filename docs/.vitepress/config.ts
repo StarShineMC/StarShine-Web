35232233616c43618c42d45f729ff6df2450c900
Xk233a <Xk233a@users.noreply.github.com>
2025-07-29 23:40:43 +0800

    3

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -41,10 +41,10 @@ export default defineConfig({
       label: '目录',
     },
 
-    socialLinks: [{ icon: 'github', link: 'https://github.com/maomao1996/vitepress-nav-template' }],
+    socialLinks: [{ icon: 'github', link: 'https://github.com/Xk233a/StarShine-Web' }],
 
     footer: {
-      message: '本站由StarShine工作室二次开发，经vitepress模板修改而来<br>致谢：<a href="https://github.com/maomao1996/vitepress-nav-template" target="_blank" rel="noopener">原源码</a>',
+      message: '本站由StarShine工作室二次开发，经vitepress模板修改而来<br>致谢：<a href="https://github.com/Xk233a/StarShine-Web" target="_blank" rel="noopener">原源码</a>',
       copyright: 'Copyright © 2024-present StarShineMC',
     },
 
